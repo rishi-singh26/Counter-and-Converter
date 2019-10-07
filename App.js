e3a009b2beb696291e093a77e9d0dcc00fdeb617
rishi <rishisingh0831@gmail.com>
2019-10-07 20:33:47 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Main from "./components/mainComponent";
+
+export default function App() {
+   return <Main />;
+}
+
+const styles = StyleSheet.create({
+   container: {
+      flex: 1,
+      backgroundColor: "#fff",
+      alignItems: "center",
+      justifyContent: "center"
+   }
+});
